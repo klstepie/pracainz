d039d34383952506159377150b410c18912255ce
Klaudia Stępień <107317111+klstepie@users.noreply.github.com>
2023-01-06 13:44:25 +0100

    Update przykladowe_dane.sql

--- przykladowe_dane.sql
+++ przykladowe_dane.sql
@@ -1,1 +1,13 @@
---dane
+-- Dane używane podczas testowania
+
+-- Tabela typy gości
+INSERT TypyGosci VALUES 
+(1, N'Turysta rodzinny'),
+(2, N'Gość biznesowy'),
+(3, N'Gość z zagranicy'),
+(4, N'Stały klient/VIP'),
+(5, N'Gość indywidualny')
+
+-- Tabela goście
+
+
