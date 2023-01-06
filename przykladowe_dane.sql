da2470f2f11fe2c65407e7aa4c711b781cffb35a
Klaudia Stępień <107317111+klstepie@users.noreply.github.com>
2023-01-06 14:39:24 +0100

    Update przykladowe_dane.sql

--- przykladowe_dane.sql
+++ przykladowe_dane.sql
@@ -88,6 +88,130 @@ VALUES (1, N'Wolny', N'Pokój jest posprzątany i gotowy na przyjęcie gościa.'
 (3, N'Wolny - Na czysto', N'Wymaga sprzątania po zwolnieniu pokoju przez gościa.'),
 (4, N'Zajęty - Przy gościu', N'Pokój jest sprzątany w trakcie pobytu gościa.')
 
+-- Tabela typy pokoi
+
+INSERT TypyPokoi 
+VALUES (1, N'Jedynka', 359, N'Pokój jednoosobowy (single room) z pojedynczym łóżkiem.'),
+(2, N'Dwójka', 449, N'Pokój dwuosobowy (double room) z jednym podwójnym łóżkiem.'),
+(3, N'Podwójny', 449, N'Pokój dwuosobowy (twin room) z dwoma osobnymi łóżkami.'),
+(4, N'Trójka', 569, N'Pokój trzyosobowy (triple room) z trzema osobnymi łóżkami.'),
+(5, N'Rodzinny', 650, N'Pokój dla co najmniej 2 osób dorosłych i 2 dzieci.'),
+(6, N'Deluxe Single', 459, N'Pokój jednoosobowy o podwyższonym standardzie.'),
+(7, N'Deluxe Double', 519, N'Pokój dwuosobowy o podwyższonym standardzie.'),
+(8, N'Apartament', 858, N'Apartament dwupokojowy z łóżkiem małżeńskim.')
+
+-- Tabela pokoje
+
+INSERT Pokoje
+VALUES (1, N'1', 1, N'1', 2),
+(2, N'2', 1, N'9', 3),
+(3, N'1', 2, N'2', 1),
+(4, N'2', 2, N'10', 1),
+(5, N'1', 3, N'3', 3),
+(6, N'2', 3, N'11', 3),
+(7, N'1', 4, N'4', 1),
+(8, N'2', 4, N'12', 1),
+(9, N'1', 5, N'5', 1),
+(10, N'2', 5, N'13', 1),
+(11, N'1', 6, N'6', 1),
+(12, N'2', 6, N'14', 1),
+(13, N'1', 7, N'7', 1),
+(14, N'2', 7, N'15', 1),
+(15, N'1', 8, N'8', 3),
+(16, N'2', 8, N'16', 1)
+
+-- Tabela pokoje - rezerwacja
+
+SET IDENTITY_INSERT [dbo].[PokojeRezerwacja] ON 
+INSERT [dbo].[PokojeRezerwacja] ([ID_Rezerwacja], [ID_Pokoj], [Id_PokojRezerwacja]) 
+VALUES (1, 1, 1),
+(2, 3, 2),
+(3, 9, 3),
+(4, 15, 4),
+(5, 12, 5),
+(6, 13, 6),
+(7, 2, 7),
+(4, 4, 8),
+(8, 11, 9),
+(9, 11, 10),
+(10, 1, 11),
+(12, 1, 14),
+(14, 2, 16),
+(15, 16, 17),
+(16, 5, 18),
+(16, 6, 19),
+(17, 1, 20)
+SET IDENTITY_INSERT [dbo].[PokojeRezerwacja] OFF
+
+-- Tabela działy
+
+INSERT Dzialy
+VALUES (N'PS1', N'Sluzba parterowa', N'Dział zajmujący się obsługą gości, dbający o bezpieczeństwo i opiekę nad gościem.'),
+(N'R1 ', N'Recepcja', N'Dział zajmujący się obsługą gościa w recepcji.'),
+(N'SP1', N'Sluzba pieter', N'Dział zajmujący się utrzymaniem czystości w pokojach i przygotowaniem ich na przyjazd gości.'),
+(N'UD1', N'Uslugi Dodatkowe', N'Dział świadczenia usług sportowo-rekreacyjnych.')
+
+-- Tabela pracownicy 
+
+INSERT Pracownicy 
+VALUES (1, N'Jan', N'Kowalski', N'Parkingowa/y', N'ul. Motylkowa 2', N'Wrocław', N'00-000', N'Polska', N'876321123', N'11111111111', N'Zatrudniony', N'PS1'),
+ (2, N'Maria', N'Nowak', N'Pokojowa/y', N'ul. Różana 2', N'Rzeszów', N'00-001', N'Polska', N'876321111', N'22222222222', N'Zatrudniony', N'SP1'),
+(3, N'Monika', N'Wichura', N'Pokojowa/y', N'ul. Bratkowa 2', N'Kraków', N'00-002', N'Polska', N'876321122', N'33333333333', N'Zatrudniony', N'SP1'),
+(4, N'Łukasz', N'Biegun', N'Ratownik', N'ul. Irysowa 2', N'Hel', N'00-003', N'Polska', N'876321133', N'44444444444', N'Zatrudniony', N'UD1'),
+(5, N'Józef', N'Stachowski', N'Masażysta/ka', N'ul. Rzepakowa 2', N'Wrocław', N'00-004', N'Polska', N'876321144', N'55555555555', N'Zatrudniony', N'UD1'),
+(6, N'Anna', N'Woźniak', N'Kierownik recepcji', N'ul. Ziemniaczana 2', N'Wrocław', N'00-005', N'Polska', N'876321155', N'66666666666', N'Zatrudniony', N'R1 '),
+(7, N'Krzysztof', N'Pąk', N'Recepcjonista/ka', N'ul. Marchewkowa 2', N'Wrocław', N'00-006', N'Polska', N'876321166', N'77777777777', N'Zatrudniony', N'R1 '),
+(8, N'Marian', N'Oleksy', N'Recepcjonista/ka', N'ul. Kościuszki 21', N'Nowy Targ', N'11-111', N'Polska', N'982664123', N'76312312312', N'Zatrudniony', N'R1 ')
+
+-- Tabela usługi
+
+INSERT Uslugi
+VALUES (1, N'Bio Sauna', 30, N'H', N'Biosauna to sauna o niższej niż w większości saun temperaturze i suchej wilgotności. W tej saunie wody nie polewa się bezpośrednio na kamienie, a mieszanki zapachowe odparowują z parownika znajdującego się na piecu.', NULL),
+(2, N'Basen', 50, N'H', N'Zewnętrzny, całoroczny basen rekreacyjny o wymiarach 5 x 10 m.', NULL),
+(3, N'VIP Jacuzzi', 299, N'H', N'Romantyczne chwile spedzone w jacuzzi – wynajem na wyłączność (godz. 21:00-22:00) wraz z owocowym poczęstunkiem', NULL),
+(4, N'Wynajem samochodu', 90, N'D', N'Wypożyczenie samochodu', N'<Informacje><NazwaUslugi>Wynajem samochodu</NazwaUslugi><NazwaFirmy>Wynajmowalnia S.A.</NazwaFirmy><Oferta><Samochod id="1"><NazwaSamochodu>Skoda Superb</NazwaSamochodu><Rocznik>2020</Rocznik><Opis>Automatyczna skrzynia biegów, liczba miejsc: 5, kolor: czarny, rodzaj paliwa: Diesel</Opis><CenaZaDobe>70</CenaZaDobe></Samochod><Samochod id="2"><NazwaSamochodu>Opel Corsa</NazwaSamochodu><Rocznik>2021</Rocznik><Opis>Manualna skrzynia biegów, liczba miejsc: 5, kolor: biały, rodzaj paliwa: Benzyna</Opis><CenaZaDobe>50</CenaZaDobe></Samochod><Samochod id="3"><NazwaSamochodu>Kia Rio</NazwaSamochodu><Rocznik>2015</Rocznik><Opis>Manualna skrzynia biegów, liczba miejsc: 5, kolor: srebrny, rodzaj paliwa: Benzyna + LPG</Opis><CenaZaDobe>40</CenaZaDobe></Samochod><DodatkoweWarunki>
+Bezpłatne odwołanie do 48 godzin;
+Ubezpieczenie od kradzieży;
+Ubezpieczenie od uszkodzeń;
+Ubezpieczenie OC;
+</DodatkoweWarunki></Oferta></Informacje>'),
+(5, N'Wynajem sali konferencyjnej', 150, N'H', N'Sala składająca się z dwóch modułów, przedzielonych ścianą rozsuwną, z bezpośrednim wyjściem do ogrodu z grillem.', NULL),
+(6, N'Parking strzeżony', 20, N'D', N'Parking strzeżony', NULL),
+(7, N'Masaż relaksujący', 289, N'H', N'Jest to bardzo delikatny zabieg, prowadzony w spokojnej, wyciszającej atmosferze, która pozwala pacjentowi oderwać się od problemów i rozluźnić się w pełni.', NULL),
+(8, N'Masaż kamieniami', 289, N'H', N'Masaż gorącymi kamieniami działa fizjoterapeutycznie. Przywraca sprawność uszkodzonym narządom, a jednocześnie sprzyja wypoczynkowi i regeneracji. Tym samym zwiększa zdolność wysiłkową organizmu.', NULL),
+(9, N'Masaż bańką chińską', 159, N'H', N'Masaż bańką chińską działa niczym drenaż limfatyczny. Poprawia krążenie krwi, przyspiesza metabolizm i usuwa nadmiar toksycznych substancji', NULL),
+(10, N'Kąpiel błotna', 120, N'H', N'Kąpiel błotna to doskonały zabieg SPA, regenerujący ciało, a także korzystnie wpływający na zdrowie. Wykorzystuje się do nich borowinę.', NULL),
+(11, N'Zabieg Odmładzająco-Liftingujący', 300, N'H', N'Intensywnie nawilżający zabieg przeznaczony do skór odwodnionych.', NULL)
+
+-- Tabela rezerwacji usług
+
+SET IDENTITY_INSERT [dbo].[RezerwacjaUslug] ON 
+INSERT [dbo].[RezerwacjaUslug] ([ID_Rezerwacja], [ID_Usluga], [DataRozpoczecia], [ID_Pracownik], [Opis], [DataZakonczenia], [ID_RezerwacjaUslugi]) 
+VALUES (5, 7, CAST(N'2023-05-04T13:30:00' AS SmallDateTime), 5, NULL, CAST(N'2023-05-04T14:30:00' AS SmallDateTime), 1),
+(5, 6, CAST(N'2023-05-02T00:00:00' AS SmallDateTime), 1, NULL, CAST(N'2023-05-10T00:00:00' AS SmallDateTime), 2),
+(3, 9, CAST(N'2023-05-05T14:30:00' AS SmallDateTime), 5, NULL, CAST(N'2023-05-05T15:30:00' AS SmallDateTime), 3),
+(6, 2, CAST(N'2023-05-07T12:00:00' AS SmallDateTime), 4, NULL, CAST(N'2023-05-07T14:00:00' AS SmallDateTime), 4),
+(10, 8, CAST(N'2023-05-08T14:00:00' AS SmallDateTime), 5, NULL, CAST(N'2023-05-08T15:00:00' AS SmallDateTime), 5),
+(12, 8, CAST(N'2022-08-17T14:00:00' AS SmallDateTime), 5, NULL, CAST(N'2022-08-17T15:00:00' AS SmallDateTime), 7),
+(14, 8, CAST(N'2022-08-16T14:00:00' AS SmallDateTime), 5, NULL, CAST(N'2022-08-16T15:00:00' AS SmallDateTime), 9),
+(17, 7, CAST(N'2022-10-05T10:00:00' AS SmallDateTime), 5, NULL, CAST(N'2022-10-05T12:00:00' AS SmallDateTime), 10),
+(17, 7, CAST(N'2022-10-06T10:00:00' AS SmallDateTime), 5, NULL, CAST(N'2022-10-06T12:00:00' AS SmallDateTime), 11),
+(17, 7, CAST(N'2022-10-07T10:00:00' AS SmallDateTime), 5, NULL, CAST(N'2022-10-07T12:00:00' AS SmallDateTime), 12),
+(17, 7, CAST(N'2022-10-08T10:00:00' AS SmallDateTime), 5, NULL, CAST(N'2022-10-08T12:00:00' AS SmallDateTime), 13),
+(17, 7, CAST(N'2022-10-09T10:00:00' AS SmallDateTime), 5, NULL, CAST(N'2022-10-09T12:00:00' AS SmallDateTime), 14),
+(17, 6, CAST(N'2022-10-04T00:00:00' AS SmallDateTime), 1, NULL, CAST(N'2022-10-10T00:00:00' AS SmallDateTime), 15),
+(17, 2, CAST(N'2022-10-05T18:00:00' AS SmallDateTime), 4, NULL, CAST(N'2022-10-05T21:00:00' AS SmallDateTime), 16),
+(17, 2, CAST(N'2022-10-06T18:00:00' AS SmallDateTime), 4, NULL, CAST(N'2022-10-06T21:00:00' AS SmallDateTime), 17),
+(17, 2, CAST(N'2022-10-07T18:00:00' AS SmallDateTime), 4, NULL, CAST(N'2022-10-07T21:00:00' AS SmallDateTime), 18),
+(17, 2, CAST(N'2022-10-08T18:00:00' AS SmallDateTime), 4, NULL, CAST(N'2022-10-08T21:00:00' AS SmallDateTime), 19),
+(17, 2, CAST(N'2022-10-09T18:00:00' AS SmallDateTime), 4, NULL, CAST(N'2022-10-09T21:00:00' AS SmallDateTime), 20)
+SET IDENTITY_INSERT [dbo].[RezerwacjaUslug] OFF
+
+
+--
+
+
+
 
 
 
